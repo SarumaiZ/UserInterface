28595522d193f440b32e046d5ae14868ceb43c33
Matheus Silva <matheuszaitune@hotmail.com>
2022-05-25 01:02:26 -0300

    Otimizando os métodos

--- controller.py
+++ controller.py
@@ -8,159 +8,6 @@ import tela_inicial
 import calc_tradicional
 import calc_cientifica
 
-def operadores(texto, sender):
-    if(sender=="√"):
-        if(texto[len(texto)-1] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",")","π"]):
-            envia = texto + "*√("
-        elif(texto[len(texto)-1] in ["+", "-", "*", "÷", " ","("]):
-            envia = texto + "√("
-        elif(texto[len(texto)-1]==","):
-            envia = texto + "0*√("
-    elif(sender=="x^"):
-        if(texto[len(texto)-1] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",")","π"]):
-            envia = texto + "^("
-        elif(texto[len(texto)-1] in ["+", "-", "*", "÷", " ","("]):
-            envia = texto
-        elif(texto[len(texto)-1]==","):
-            envia = texto + "0^("
-    elif(texto[len(texto)-1] in ["+", "-", "*", "÷"]):
-        if(texto[len(texto)-2]=="(" and sender in ["+","-"]):
-            envia = texto[0:len(texto)-1]+sender
-        elif(texto[len(texto)-2] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",")","π"]):
-            envia = texto[0:len(texto)-1]+sender
-        else:
-            envia = texto
-    elif(texto[len(texto)-1] == ","):
-        envia = texto + "0" + sender
-    elif(texto[len(texto)-1] == "("):
-        if(sender in ["+", "-"]):
-            envia = texto + sender
-        else:
-            envia = texto
-    elif(texto == " "):
-        envia = texto
-    else:
-        envia = texto + sender
-    return envia
-
-def virgula(texto):
-    if(texto == " " or texto[len(texto)-1] in ["+", "-", "*", "√", "÷", "("]):
-        envia = texto+"0,"
-    elif(texto[len(texto)-1] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","π"]):
-        numeros = [""]
-        i = 0
-        for dig in texto:
-            if(dig in ["+", "-", "*", "÷", "√"]):
-                numeros.append("")
-            numeros[i] = numeros[i]+dig
-            if(dig in ["+", "-", "*", "÷", "√"]):
-                numeros[i] = numeros[i][:len(numeros[i])-1]
-                i += 1
-        for alg in numeros[i]:
-            if(alg == ','):
-                envia = texto
-                break
-            else:
-                envia = texto+","
-    elif(texto[len(texto)-1] == ","):
-        envia = texto
-    else:
-        envia = texto
-    return envia
-
-def abrep(texto):
-    for dig in texto:
-        if(dig in ["+", "-", "*", "÷", "√", "(", " "]):
-            envia = texto+"("
-        elif(dig in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ")","π"]):
-            envia = texto+"*("
-    if(texto == ""):
-        envia = texto
-    elif(texto[len(texto)-1] == ","):
-        envia = texto+"0*("
-    return envia
-
-def fechap(texto):
-    contab = 0
-    contfe = 0
-    for dig in texto:
-        if(dig == "("):
-            contab += 1
-        if(dig == ")"):
-            contfe += 1
-    if(contab-contfe == 0 or texto[len(texto)-1] in ["+", "-", "*", "÷", "(", " "]):
-        envia = texto
-    elif(texto[len(texto)-1]==","):
-        envia = texto + "0)"
-    elif(contab-contfe >= 1):
-        envia = texto+")"
-    else:
-        envia = texto
-    return envia
-
-def porcentagem(texto):
-    # texto=str(float(texto)/100)
-    if(texto == " " or texto[len(texto)-1] in ["+", "-", "*", "√", "÷", "(", " ", ")", "%"]):
-        envia = texto
-    elif(texto[len(texto)-1] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","π"]):
-        envia = texto+"%"
-    else:
-        envia = texto
-    return envia
-
-def angulos(texto, sender):
-    if(texto[len(texto)-1] in ["+", "-", "*", "÷", "(", " ", "o"]):
-        envia = texto+sender+"("
-    elif(texto[len(texto)-1] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ")","π"]):
-        envia = texto+"*"+sender+"("
-    else:
-        envia = texto+sender+"("
-    return envia
-
-def apaga(texto):
-    envia = texto[0:len(texto)-1]
-    if(texto == " "):
-        envia = " "
-    elif(texto[len(texto)-1]=="(" and texto[len(texto)-2] in ["√", "^"]):
-        envia = texto[0:len(texto)-2]
-    elif(texto[len(texto)-1]=="(" and texto[len(texto)-2] in ["n", "s"]):
-        envia = texto[0:len(texto)-4]
-    return envia
-
-def escrever(texto, sender):
-    if(texto[len(texto)-1]==")"):
-        envia = texto + "*" + sender
-    else:
-        envia = texto+sender
-    return envia
-
-def igual(texto):
-    i=0
-    j=0
-    for dig in texto:
-        if(dig==","):
-            texto = texto[:i]+"."+texto[i+1:]
-        if(dig=="e"):
-            texto = texto[:i]+"i"+texto[i+1:]
-        if(dig=="√"):
-            texto = texto[:i]+"sqrt"+texto[i+1:]
-            i+=3
-        if(dig=="^"):
-            texto = texto[:i]+"**"+texto[i+1:]
-            i+=1
-        if(dig=="÷"):
-            texto = texto[:i]+"/"+texto[i+1:]
-        if(dig=="π"):
-            texto = texto[:i]+"pi"+texto[i+1:]
-            i+=1
-        i+=1
-    envia = str(eval(texto))
-    for dig in envia:
-        if(dig=="."):
-            envia = envia[:j]+","+envia[j+1:]
-        j+=1
-    return envia
-
 class controller:
     def __init__(self):
         self.tela_inicial_Window = QtWidgets.QMainWindow()
@@ -177,6 +24,7 @@ class controller:
 
         self.tela_inicial_ui.pushButton.clicked.connect(self.show_calc)
 
+        # Calculadora tradicional
         # Botões algarismos calculadora tradicional
         self.tradicional_ui.pushButton_19.clicked.connect(self.escreve)  # 0
         self.tradicional_ui.pushButton_13.clicked.connect(self.escreve)  # 1
@@ -195,7 +43,7 @@ class controller:
         self.tradicional_ui.pushButton_10.clicked.connect(self.operadores)  # *
         self.tradicional_ui.pushButton_9.clicked.connect(self.operadores)  # ÷
         self.tradicional_ui.pushButton.clicked.connect(self.operadores)  # √
-        self.tradicional_ui.pushButton_21.clicked.connect(self.porco)  # %
+        self.tradicional_ui.pushButton_21.clicked.connect(self.porcentagem)  # %
         self.tradicional_ui.pushButton_18.clicked.connect(self.virgula)  # ,
 
         # Botões parênteses calculadora tradicional
@@ -212,6 +60,7 @@ class controller:
         self.tradicional_ui.pushButton_4.clicked.connect(self.apaga)
         self.tradicional_ui.pushButton_5.clicked.connect(self.apagaDEL)
 
+        # Calculadora científica
         # Botões algarismos calculadora científica
         self.cientifica_ui.pushButton_17.clicked.connect(self.escreve)  # π
         self.cientifica_ui.pushButton_19.clicked.connect(self.escreve)  # 0
@@ -232,7 +81,7 @@ class controller:
         self.cientifica_ui.pushButton_23.clicked.connect(self.operadores)  # ÷
         self.cientifica_ui.pushButton_10.clicked.connect(self.operadores)  # √
         self.cientifica_ui.pushButton_20.clicked.connect(self.operadores)  # x^
-        self.cientifica_ui.pushButton_27.clicked.connect(self.porco)  # %
+        self.cientifica_ui.pushButton_27.clicked.connect(self.porcentagem)  # %
         self.cientifica_ui.pushButton.clicked.connect(self.angulo)  # SEN
         self.cientifica_ui.pushButton_9.clicked.connect(self.angulo)  # COS
         self.cientifica_ui.pushButton_22.clicked.connect(self.angulo)  # TAN
@@ -254,87 +103,202 @@ class controller:
         self.cientifica_ui.pushButton_5.clicked.connect(self.apagaDEL)
     
     # Métodos
-    def textos(self):
+    def porcentagem(self):
+        if(self.tradicional_Window.isActiveWindow()):
+            texto = self.tradicional_ui.label.text()
+        elif(self.cientifica_Window.isActiveWindow()):
+            texto = self.cientifica_ui.label.text()
+
+        # texto=str(float(texto)/100)
+        if(texto == " " or texto.endswith(("+", "-", "*", "√", "÷", "(", " ", ")", "%"))):
+            envia = texto
+        elif(texto.endswith(("0", "1", "2", "3", "4", "5", "6", "7", "8", "9","π"))):
+            envia = texto+"%"
+        else:
+            envia = texto
+
         # Calculadora tradicional
-        self.texto = self.tradicional_ui.label.text()
-        self.sender = self.tradicional_Window.sender().text()
+        self.tradicional_ui.label.setText(envia)
 
         # Calculadora científica
-        self.textoc = self.cientifica_ui.label.text()
-        self.senderc = self.cientifica_Window.sender().text()
-
-    def porco(self):
-        self.textos()
-
-        # Calculadora tradicional
-        self.tradicional_ui.label.setText(porcentagem(self.texto))
-
-        # Calculadora científica
-        self.cientifica_ui.label.setText(porcentagem(self.textoc))
+        self.cientifica_ui.label.setText(envia)
 
     def operadores(self):
-        self.textos()
+        if(self.tradicional_Window.isActiveWindow()):
+            texto = self.tradicional_ui.label.text()
+            sender = self.tradicional_Window.sender().text()
+        elif(self.cientifica_Window.isActiveWindow()):
+            texto = self.cientifica_ui.label.text()
+            sender = self.cientifica_Window.sender().text()
+        
+        if(sender=="√"):
+            if(texto.endswith(("0", "1", "2", "3", "4", "5", "6", "7", "8", "9","π",")"))):
+                envia = texto + "*√("
+            elif(texto.endswith(("+", "-", "*", "÷", " ","("))):
+                envia = texto + "√("
+            elif(texto.endswith(",")):
+                envia = texto + "0*√("
+        elif(sender=="x^"):
+            if(texto.endswith(("0", "1", "2", "3", "4", "5", "6", "7", "8", "9","π",")"))):
+                envia = texto + "^("
+            elif(texto.endswith(("+", "-", "*", "÷", " ","("))):
+                envia = texto
+            elif(texto.endswith(",")):
+                envia = texto + "0^("
+        elif(texto.endswith(("+", "-", "*", "÷"))):
+            if(texto[len(texto)-2]=="(" and sender in ["+","-"]):
+                envia = texto[0:len(texto)-1]+sender
+            elif(texto[:len(texto)-1].endswith(("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",")","π"))):
+                envia = texto[0:len(texto)-1]+sender
+            else:
+                envia = texto
+        elif(texto.endswith(",")):
+            envia = texto + "0" + sender
+        elif(texto.endswith("(")):
+            if(sender in ["+", "-"]):
+                envia = texto + sender
+            else:
+                envia = texto
+        elif(texto == " "):
+            envia = texto
+        else:
+            envia = texto + sender
         
         # Calculadora tradicional
-        self.tradicional_ui.label.setText(operadores(self.texto, self.sender))
+        self.tradicional_ui.label.setText(envia)
 
         # Calculadora científica
-        self.cientifica_ui.label.setText(operadores(self.textoc, self.senderc))
+        self.cientifica_ui.label.setText(envia)
 
     def virgula(self):
-        self.textos()
+        if(self.tradicional_Window.isActiveWindow()):
+            texto = self.tradicional_ui.label.text()
+        elif(self.cientifica_Window.isActiveWindow()):
+            texto = self.cientifica_ui.label.text()
+
+        if(texto == " " or texto.endswith(("+", "-", "*", "√", "÷", "("))):
+            envia = texto+"0,"
+        elif(texto.endswith(("0", "1", "2", "3", "4", "5", "6", "7", "8", "9","π"))):
+            index = max(texto.rfind("+"), texto.rfind("-"), texto.rfind("*"), texto.rfind("÷"), texto.rfind("√"), texto.rfind("^"))+1
+            if(texto[index:].find(",")==-1):
+                envia=texto+","
+            else:
+                envia=texto
+        elif(texto.endswith(",")):
+            envia = texto
+        else:
+            envia = texto
 
         # Calculadora tradicional
-        self.tradicional_ui.label.setText(virgula(self.texto))
+        self.tradicional_ui.label.setText(envia)
 
         # Calculadora científica
-        self.cientifica_ui.label.setText(virgula(self.textoc))
+        self.cientifica_ui.label.setText(envia)
 
     def abre(self):
-        self.textos()
+        if(self.tradicional_Window.isActiveWindow()):
+            texto = self.tradicional_ui.label.text()
+        elif(self.cientifica_Window.isActiveWindow()):
+            texto = self.cientifica_ui.label.text()
+
+        if(texto.endswith(("+", "-", "*", "÷", "√", "(", " "))):
+            envia = texto+"("
+        elif(texto.endswith(("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ")","π"))):
+            envia = texto+"*("
+        elif(texto.endswith(",")):
+            envia = texto+"0*("
+        else:
+            envia = texto
 
         # Calculadora tradicional
-        self.tradicional_ui.label.setText(abrep(self.texto))
+        self.tradicional_ui.label.setText(envia)
 
         # Calculadora científica
-        self.cientifica_ui.label.setText(abrep(self.textoc))
+        self.cientifica_ui.label.setText(envia)
 
     def fecha(self):
-        self.textos()
+        if(self.tradicional_Window.isActiveWindow()):
+            texto = self.tradicional_ui.label.text()
+        elif(self.cientifica_Window.isActiveWindow()):
+            texto = self.cientifica_ui.label.text()
+
+        contab = texto.count("(")
+        contfe = texto.count(")")
+        if(contab-contfe == 0 or texto.endswith(("+", "-", "*", "÷", "(", " "))):
+            envia = texto
+        elif(texto.endswith(",")):
+            envia = texto + "0)"
+        elif(contab-contfe >= 1):
+            envia = texto+")"
+        else:
+            envia = texto
 
         # Calculadora tradicional
-        self.tradicional_ui.label.setText(fechap(self.texto))
+        self.tradicional_ui.label.setText(envia)
 
         # Calculadora científica
-        self.cientifica_ui.label.setText(fechap(self.textoc))
+        self.cientifica_ui.label.setText(envia)
 
     def angulo(self):
-        self.textos()
+        texto = self.cientifica_ui.label.text()
+        sender = self.cientifica_Window.sender().text()
 
-        self.cientifica_ui.label.setText( angulos(self.textoc, self.senderc) )
+        if(texto.endswith(("+", "-", "*", "÷", "(", " ", "a"))):
+            envia = texto+sender+"("
+        elif(texto.endswith(("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ")","π"))):
+            envia = texto+"*"+sender+"("
+        else:
+            envia = texto+sender+"("
+
+        self.cientifica_ui.label.setText(envia)
 
     def arco(self):
-        self.textos()
+        texto = self.cientifica_ui.label.text()
 
-        self.cientifica_ui.label.setText( self.textoc + self.senderc )
+        self.cientifica_ui.label.setText(texto+"a")
 
     def escreve(self):
-        self.textos()
+        if(self.tradicional_Window.isActiveWindow()):
+            texto = self.tradicional_ui.label.text()
+            sender = self.tradicional_Window.sender().text()
+        elif(self.cientifica_Window.isActiveWindow()):
+            texto = self.cientifica_ui.label.text()
+            sender = self.cientifica_Window.sender().text()
+
+        if(texto.endswith(")")):
+            envia = texto + "*" + sender
+        else:
+            envia = texto+sender
 
         # Calculadora tradicional
-        self.tradicional_ui.label.setText(escrever(self.texto, self.sender))
+        self.tradicional_ui.label.setText(envia)
 
         # Calculadora científica
-        self.cientifica_ui.label.setText(escrever(self.textoc, self.senderc))
+        self.cientifica_ui.label.setText(envia)
 
     def resultado(self):
-        self.textos()
+        if(self.tradicional_Window.isActiveWindow()):
+            texto = self.tradicional_ui.label.text()
+        elif(self.cientifica_Window.isActiveWindow()):
+            texto = self.cientifica_ui.label.text()
+
+        contab = texto.count("(")
+        contfe = texto.count(")")
+        if(contab-contfe >= 1):
+            texto = texto+")"*(contab-contfe)
+
+        texto = texto.replace(",",".").replace("e","i").replace("√","sqrt").replace("^","**").replace("÷","/").replace("π","pi")
+        try:
+            envia = str(eval(texto)).replace(".",",")
+        except (NameError, ValueError, SyntaxError):
+            envia = texto.replace(".",",").replace("pi","π").replace("i","e").replace("sqrt","√").replace("**","^").replace("/","÷")
+            QtWidgets.QMessageBox.about(self.cientifica_Window, "Math domain error", "Raízes negativas não são permitidas\nArcos (seno e cosseno) são calculados para valores entre -1 e +1")
 
         # Calculadora tradicional
-        self.tradicional_ui.label.setText(igual(self.texto))
+        self.tradicional_ui.label.setText(envia)
 
         # Calculadora científica
-        self.cientifica_ui.label.setText(igual(self.textoc))
+        self.cientifica_ui.label.setText(envia)
 
     def apagaDEL(self):
         # Calculadora tradicional
@@ -344,13 +308,24 @@ class controller:
         self.cientifica_ui.label.setText(" ")
 
     def apaga(self):
-        self.textos()
+        if(self.tradicional_Window.isActiveWindow()):
+            texto = self.tradicional_ui.label.text()
+        elif(self.cientifica_Window.isActiveWindow()):
+            texto = self.cientifica_ui.label.text()
+        
+        envia = texto[0:len(texto)-1]
+        if(texto == " "):
+            envia = " "
+        elif(texto.endswith("(") and texto[:len(texto)-1].endswith(("√", "^"))):
+            envia = texto[0:len(texto)-2]
+        elif(texto.endswith("(") and texto[:len(texto)-1].endswith(("n", "s"))):
+            envia = texto[0:len(texto)-4]
 
         # Calculadora tradicional
-        self.tradicional_ui.label.setText(apaga(self.texto))
+        self.tradicional_ui.label.setText(envia)
 
         # Calculadora científica
-        self.cientifica_ui.label.setText(apaga(self.textoc))
+        self.cientifica_ui.label.setText(envia)
 
     def voltar(self):
         self.tradicional_Window.close()
@@ -370,6 +345,7 @@ class controller:
         if self.tela_inicial_ui.radioButton_2.isChecked():
             self.cientifica_Window.show()
             self.tela_inicial_Window.close()
+            QtWidgets.QMessageBox.about(self.cientifica_Window, "Aviso", "Os ângulos são calculados em radianos")
 
         if not(self.tela_inicial_ui.radioButton.isChecked()) and not(self.tela_inicial_ui.radioButton_2.isChecked()):
             QtWidgets.QMessageBox.about(self.tela_inicial_Window, "Erro", "Escolha uma opção!")
--- teste.py
+++ teste.py
@@ -2,21 +2,42 @@ import os
 from math import *
 os.system('cls')
 
-"""texto="50"
+'''texto="50"
 aux=""
 for dig in texto:
     if(dig not in ["+", "-", "X", "√", "÷"]):
         carac=str(float(texto)/100)
 print(carac)
 print(type(carac))
-print(texto[0])"""
+print(texto[0])'''
 
-texto = "3*√(9*√(4))"
-#texto=""
-i=0
+'''x=texto.find("u")
+#print(degrees(atan(sqrt(3)/3))+0.1)
+if(x):
+    print(x)
+    print(texto)
+else:
+    print(x)
+    print(texto)'''
+texto="2,0+0,1"
+index = max(texto.rfind("+"), texto.rfind("-"), texto.rfind("*"), texto.rfind("÷"), texto.rfind("√"), texto.rfind("^"))+1
+if(texto[index:].find(",")==-1):
+    envia=texto+","
+else:
+    envia=texto
+
+'''numeros = [""]
+i = 0
 for dig in texto:
-    if(dig=="√"):
-        texto = texto[:i]+"sqrt"+texto[i+1:]
-        i+=3
-    i+=1
-print((texto))
+    if(dig in ["+", "-", "*", "÷", "√"]):
+        numeros.append("")
+    numeros[i] = numeros[i]+dig
+    if(dig in ["+", "-", "*", "÷", "√"]):
+        numeros[i] = numeros[i][:len(numeros[i])-1]
+        i += 1
+for alg in numeros[i]:
+    if(alg == ','):
+        envia = texto
+        break
+    else:
+        envia = texto+","'''
